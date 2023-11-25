2f3132cf1a42f9329273c8a0ac674e4168feac1d
João Alves <joao.2003.couto@gmail.com>
2023-11-25 13:25:40 +0000

    Added lombok

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module prog.projeto {
   requires org.controlsfx.controls;
   requires net.synedra.validatorfx;
   requires org.kordamp.bootstrapfx.core;
+  requires static lombok;
 
   opens prog.projeto to javafx.fxml;
   exports prog.projeto;
