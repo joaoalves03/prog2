68b3c9ccdb5acce495c66b97848b00a39537eec0
João Alves <joao.2003.couto@gmail.com>
2023-12-18 14:51:09 +0000

    Added Appointment model

---
+++ src/main/java/prog/projeto/models/Extra.java
@@ -0,0 +1,12 @@
+package prog.projeto.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Extra {
+  String name;
+  float price;
+  int quantity;
+}
