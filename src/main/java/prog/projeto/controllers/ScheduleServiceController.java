4753c62e87d3dfe3ed0022aca61763850098385e
Pedro Cunha <pedro10cunha@gmail.com>
2023-12-31 04:00:36 +0000

    Fixed Client Pages not switching scene

--- src/main/java/prog/projeto/controllers/ScheduleServiceController.java
+++ src/main/java/prog/projeto/controllers/ScheduleServiceController.java
@@ -12,18 +12,21 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class ScheduleServiceController implements Initializable {
+public class ScheduleServiceController {
   @FXML
   private ChoiceBox<String> serviceTypeChoiceBox;
 
-  @Override
-  public void initialize(URL url, ResourceBundle resourceBundle) {
-    serviceTypeChoiceBox.setItems(
-            FXCollections.observableArrayList(
-                    AnimalServiceType.Education.description,
-                    AnimalServiceType.Walking.description,
-                    AnimalServiceType.Grooming.description
-            )
-    );
+  public void initialize() throws RuntimeException {
+    try {
+      serviceTypeChoiceBox.setItems(
+              FXCollections.observableArrayList(
+                      AnimalServiceType.Education.description,
+                      AnimalServiceType.Walking.description,
+                      AnimalServiceType.Grooming.description
+              )
+      );
+    } catch (RuntimeException e) {
+      System.out.println(e.getMessage());
+    }
   }
 }
