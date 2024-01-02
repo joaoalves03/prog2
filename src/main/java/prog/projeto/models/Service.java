bd8efcb3e14294c56adad2d040e5ed694c025ea3
João Alves <joao.2003.couto@gmail.com>
2024-01-02 02:04:01 +0000

    Services functionality should be working now

--- src/main/java/prog/projeto/models/Service.java
+++ src/main/java/prog/projeto/models/Service.java
@@ -4,9 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @AllArgsConstructor
-public class Service {
+public class Service implements Serializable {
   private int id;
   @Setter private String name;
   @Setter private float price;
