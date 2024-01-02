d47b6d6f7bff14dcb9a025adf2975effb4bb2464
João Alves <joao.2003.couto@gmail.com>
2024-01-02 15:33:38 +0000

    Added getEmployees for animal center

--- src/main/java/prog/projeto/repositories/AnimalCenterRepository.java
+++ src/main/java/prog/projeto/repositories/AnimalCenterRepository.java
@@ -1,6 +1,11 @@
 package prog.projeto.repositories;
 
 import prog.projeto.models.AnimalCenter;
+import prog.projeto.models.User;
+import prog.projeto.models.UserType;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AnimalCenterRepository extends Repository<AnimalCenter> {
   private static AnimalCenterRepository instance;
@@ -10,6 +15,18 @@ public class AnimalCenterRepository extends Repository<AnimalCenter> {
   public static AnimalCenterRepository getInstance() {
     if(instance == null) instance = new AnimalCenterRepository();
     return instance;
+  }
+
+  public List<User> getEmployees(AnimalCenter animalCenter) {
+    UserRepository userRepository = UserRepository.getInstance();
+    List<User> employees = new ArrayList<>();
+
+    employees.addAll(userRepository.getByType(UserType.Secretary));
+    employees.addAll(userRepository.getByType(UserType.Assistant));
+    employees.addAll(userRepository.getByType(UserType.Educator));
+    employees.addAll(userRepository.getByType(UserType.Veterinarian));
+
+    return employees;
   }
 
   @Override
