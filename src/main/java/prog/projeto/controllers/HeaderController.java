28d1b7c57b785612e57925a5435bd585e14a33fa
Pedro Cunha <pedro10cunha@gmail.com>
2024-01-03 00:22:44 +0000

    Updated HeaderController

--- src/main/java/prog/projeto/controllers/HeaderController.java
+++ src/main/java/prog/projeto/controllers/HeaderController.java
@@ -3,10 +3,9 @@ package prog.projeto.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
-import javafx.scene.layout.FlowPane;
-import javafx.stage.Stage;
 import prog.projeto.SceneManager;
 import prog.projeto.models.User;
 import prog.projeto.repositories.UserRepository;
@@ -15,14 +14,9 @@ public class HeaderController {
   @FXML
   MenuButton userDropDown;
 
-  @FXML
-  FlowPane closeButton;
-
   public void initialize() {
     UserRepository userRepository = UserRepository.getInstance();
     User selectedUser = userRepository.getSelectedUser();
-
-    System.out.println(selectedUser);
 
     userDropDown.setText(
         selectedUser == null
@@ -34,53 +28,28 @@ public class HeaderController {
   protected void openPage(ActionEvent event) {
     MenuItem menuItem = (MenuItem) event.getSource();
     String userData = (String) menuItem.getUserData();
-    Stage stage = SceneManager.getStage(userDropDown);
 
-    if (stage != null) {
-      try {
-        SceneManager.switchScene(stage, userData);
-      } catch (Exception e) {
-        System.out.println("SceneManager: " + e.getMessage());
-      }
-    } else {
-      System.out.println("Stage is null. Unable to switch scenes.");
-    }
+    SceneManager.switchScene(userDropDown, userData);
   }
 
   @FXML
-  protected void openModal(ActionEvent event) {
-    MenuItem menuItem = (MenuItem) event.getSource();
-    String userData = (String) menuItem.getUserData();
+  protected void openModal(ActionEvent event){
+    MenuItem node = (MenuItem) event.getSource();
+    String scenePath = (String) node.getUserData();
+    String title = node.getText();
 
-    try {
-      SceneManager.openNewModal(userData, "Settings", true);
-    } catch (Exception e) {
-      System.out.println("SceneManager: " + e.getMessage());
-    }
+    SceneManager.openNewModal(scenePath, title, true);
+    initialize();
   }
 
   @FXML
   protected void logout(){
-    Stage stage = SceneManager.getStage(userDropDown);
-
-    if (stage != null) {
-      try {
-        SceneManager.switchScene(stage, "login.fxml");
-      } catch (Exception e) {
-        System.out.println("SceneManager: " + e.getMessage());
-      }
-    } else {
-      System.out.println("Stage is null. Unable to switch scenes.");
-    }
+    SceneManager.switchScene(userDropDown, "login.fxml");
   }
 
   @FXML
-  protected void closeWindow(){
-    Stage stage = SceneManager.getStage(closeButton);
-    if (stage != null) {
-      stage.close();
-    } else {
-      System.out.println("Stage is null. Unable to close stage");
-    }
+  protected void closeWindow(ActionEvent event){
+    Node closeButton = (Node) event.getSource();
+    SceneManager.closeWindow(closeButton);
   }
 }
