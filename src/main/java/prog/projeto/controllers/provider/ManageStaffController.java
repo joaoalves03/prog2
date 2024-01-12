86f08398a96dc50ffe38272ac3508082e43ce194
João Alves <joao.2003.couto@gmail.com>
2024-01-12 23:41:54 +0000

    Now able to add and remove employees

--- src/main/java/prog/projeto/controllers/provider/ManageStaffController.java
+++ src/main/java/prog/projeto/controllers/provider/ManageStaffController.java
@@ -3,18 +3,26 @@ package prog.projeto.controllers.provider;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import prog.projeto.PetCareApplication;
 import prog.projeto.SceneManager;
+import prog.projeto.models.AnimalCenter;
 import prog.projeto.models.User;
+import prog.projeto.repositories.AnimalCenterRepository;
 import prog.projeto.repositories.UserRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class ManageStaffController {
+  @FXML
+  HBox controlButtons;
+  @FXML
+  ComboBox<AnimalCenter> animalCenterList;
   @FXML
   ListView<User> usersList;
 
@@ -28,29 +36,35 @@ public class ManageStaffController {
   @FXML
   Label address;
   @FXML
-  Label city;
-  @FXML
   Label phone;
 
 
   @FXML
   private void initialize() {
+    AnimalCenterRepository animalCenterRepository = AnimalCenterRepository.getInstance();
     UserRepository userRepository = UserRepository.getInstance();
 
-    // Set the cell factory to display the user's first name
+    try {
+      animalCenterRepository.read();
+    } catch (Exception ignored) {
+      SceneManager.openErrorAlert("Erro", "Não foi possível obter locais de recolha");
+      close();
+    }
+
+    animalCenterList.getItems().addAll(
+      animalCenterRepository.getByProvider(userRepository.getSelectedUser().getId())
+    );
+
+    animalCenterList.getSelectionModel().selectedItemProperty().addListener((options, oldValue, newValue) -> {
+      refreshList();
+      controlButtons.setVisible(true);
+    });
+
     usersList.setCellFactory(param -> new UserListCell());
-
-    // TODO: Filter by animal center
-    // Populate the ListView with users
-    usersList.getItems().addAll(userRepository.getAllUsers());
-
-    // Set the selection mode to single
     usersList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 
-    // Add a listener to detect when an item is selected
     usersList.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
       if (newValue != null) {
-        // If a user is selected, update the label text
         name.setText(newValue.getFirstName() + " " + newValue.getLastName());
         email.setText(newValue.getEmail());
         address.setText(newValue.getAddress() + ", " + newValue.getCity());
@@ -79,16 +93,15 @@ public class ManageStaffController {
       Scene scene = new Scene(fxmlLoader.load());
       Stage stage = new Stage();
       stage.setScene(scene);
-      stage.setTitle("Adicionar Empregado");
+      stage.setTitle("Adicionar Funcionário");
       stage.centerOnScreen();
       StaffFormController controller = fxmlLoader.getController();
-      controller.setAnimalCenter(usersList.getSelectionModel().getSelectedIndex());
+      controller.setAnimalCenter(animalCenterList.getSelectionModel().getSelectedItem().getId());
       stage.showAndWait();
+      refreshList();
     } catch (Exception ignored) {
-      SceneManager.openErrorAlert("Erro", "Não foi possível criar um novo empregado");
+      SceneManager.openErrorAlert("Erro", "Não foi possível criar um novo funcionário");
     }
-
-    refreshList();
   }
 
   @FXML
@@ -112,28 +125,62 @@ public class ManageStaffController {
 
   @FXML
   private void remove(){
-    /*if (currentUser == -1) {
-      SceneManager.openErrorAlert("Erro", "Selecione um Utilizador");
-      return;
+    UserRepository userRepository = UserRepository.getInstance();
+    AnimalCenterRepository animalCenterRepository = AnimalCenterRepository.getInstance();
+    User employee = usersList.getSelectionModel().getSelectedItem();
+    AnimalCenter animalCenter = animalCenterList.getSelectionModel().getSelectedItem();
+
+    Alert alert = new Alert(
+        Alert.AlertType.CONFIRMATION,
+        "Deseja mesmo apagar este funcionário?",
+        ButtonType.OK,
+        ButtonType.CANCEL
+    );
+    alert.setHeaderText(String.format(
+        "Apagar %s %s?",
+        employee.getFirstName(),
+        employee.getLastName()
+    ));
+    alert.setTitle("Apagar funcionário");
+    Optional<ButtonType> result = alert.showAndWait();
+
+    if(result.isPresent() && result.get() == ButtonType.OK) {
+      try {
+        userRepository.delete(employee.getId());
+        animalCenter.getEmployees().remove((Integer) employee.getId());
+        userRepository.save();
+        animalCenterRepository.save();
+      } catch (Exception e) {
+        SceneManager.openErrorAlert("Erro", "Não foi possível apagar o funcionário\n"+e.getMessage());
+      }
     }
 
-    boolean response = SceneManager.openConfirmationAlert("Remover utilizador", "Têm a certeza que quer eliminareste utilizador?");
-    if(!response) { return; }
-
-    UserRepository userRepository = UserRepository.getInstance();
-    userRepository.delete(currentUser);
-    try {
-      userRepository.save();
-    } catch (Exception e) {
-      System.out.println(e.getMessage());
-    }*/
+    refreshList();
   }
 
   protected void refreshList() {
     UserRepository userRepository = UserRepository.getInstance();
+    AnimalCenterRepository animalCenterRepository = AnimalCenterRepository.getInstance();
+    List<User> employees = new ArrayList<>();
 
-    // TODO: Filter users by animal center
+    // This looks repetitive but is needed to get the newest
+    // animal center information after adding an employee
+    AnimalCenter animalCenter = animalCenterRepository.findById(
+        animalCenterList.getSelectionModel().getSelectedItem().getId()
+    );
+
+    for(int id: animalCenter.getEmployees()) {
+      employees.add(
+          userRepository.findById(id)
+      );
+    }
+
     usersList.getItems().clear();
-    usersList.getItems().addAll(userRepository.getAllUsers());
+    usersList.getItems().addAll(employees);
+  }
+
+  public void close() {
+    Stage stage = (Stage) animalCenterList.getScene().getWindow();
+    stage.close();
   }
 }
