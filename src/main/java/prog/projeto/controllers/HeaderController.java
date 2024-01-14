c09c89d38ca1a2c0c62f4634c1e8b4e77220dcd6
Pedro Cunha <pedro10cunha@gmail.com>
2024-01-14 00:41:59 +0000

    Added subtitle to header

--- src/main/java/prog/projeto/controllers/HeaderController.java
+++ src/main/java/prog/projeto/controllers/HeaderController.java
@@ -4,20 +4,37 @@ package prog.projeto.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import prog.projeto.SceneManager;
 import prog.projeto.models.User;
+import prog.projeto.models.UserType;
 import prog.projeto.repositories.UserRepository;
 
 public class HeaderController {
   @FXML
   MenuButton userDropDown;
+  @FXML
+  Label subTitle;
 
   public void initialize() {
     if(userDropDown == null) return;
     UserRepository userRepository = UserRepository.getInstance();
     User selectedUser = userRepository.getSelectedUser();
+
+    String subTitleString = selectedUser.getType() == UserType.Admin
+            ? "Gestão"
+            : selectedUser.getType() == UserType.ServiceProvider || selectedUser.getType() == UserType.Staff
+            ? "Serviços"
+            : selectedUser.getType() == UserType.Client
+            ? "Clientes"
+            : null;
+
+    if(subTitleString != null ){
+      subTitle.setText(subTitleString);
+      subTitle.setManaged(true);
+    }
 
     userDropDown.setText(
         selectedUser == null
