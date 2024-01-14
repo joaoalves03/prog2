bf71a7f7a841198ad275e137ed09c9f477953ebf
João Alves <joao.2003.couto@gmail.com>
2024-01-14 17:40:45 +0000

    Extra products can now be added to an appointment

--- src/main/java/prog/projeto/models/Extra.java
+++ src/main/java/prog/projeto/models/Extra.java
@@ -3,9 +3,11 @@ package prog.projeto.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @AllArgsConstructor
 @Data
-public class Extra {
+public class Extra implements Serializable {
   private String name;
   private float price;
   private int quantity;
